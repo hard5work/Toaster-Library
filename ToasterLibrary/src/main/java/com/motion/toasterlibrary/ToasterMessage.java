31ec8e588025db6b642859d2c5d12a2c469ec343
Anish <maharjan.rule@gmail.com>
2021-08-07 09:33:45 +0545

    changes on toast

--- ToasterLibrary/src/main/java/com/motion/toasterlibrary/ToasterMessage.java
+++ ToasterLibrary/src/main/java/com/motion/toasterlibrary/ToasterMessage.java
@@ -1,9 +1,22 @@
 package com.motion.toasterlibrary;
 
+import android.app.Activity;
+import android.app.Application;
 import android.content.Context;
 import android.widget.Toast;
 
-public class ToasterMessage {
+public class ToasterMessage  extends Toast{
+    /**
+     * Construct an empty Toast object.  You must call {@link #setView} before you
+     * can call {@link #show}.
+     *
+     * @param context The context to use.  Usually your {@link Application}
+     *                or {@link Activity} object.
+     */
+    public ToasterMessage(Context context) {
+        super(context);
+    }
+
     public static void s(Context c, String message ){
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
     }
